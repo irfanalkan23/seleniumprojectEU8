7b84ecdff6ea1a5198a84a02f93e926e1e67d2cf
irfan <23pointalpha@gmail.com>
2022-05-13 15:09:19 +0300

    day4 T4 findElements

--- src/test/java/com/cydeo/tests/findElements_checkboxes_radio/T4_findElements.java
+++ src/test/java/com/cydeo/tests/findElements_checkboxes_radio/T4_findElements.java
@@ -1,8 +1,11 @@
 package com.cydeo.tests.findElements_checkboxes_radio;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class T4_findElements {
@@ -21,11 +24,18 @@ public class T4_findElements {
         driver.get("https://practice.cydeo.com/abtest");
 
         //3- Locate all the links in the page.
-
+        List<WebElement> allLinks = driver.findElements(By.tagName("a"));
 
         //4- Print out the number of the links on the page.
+        System.out.println("allLinks.size() = " + allLinks.size());
+
         //5- Print out the texts of the links.
         //6- Print out the HREF attribute values of the links
+        for (WebElement each : allLinks) {
+            System.out.println("Text of link = " + each.getText());
+            System.out.println("HREF values: " + each.getAttribute("href"));
+        }
+
 
 
 
