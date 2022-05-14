7e00b611cb7d44b3e7df4589857dee25056e9d49
irfan <23pointalpha@gmail.com>
2022-05-14 20:58:00 +0300

    day5 TestNG_Intro TestNG_Selenium

--- src/test/java/com/cydeo/tests/day5_testNG_Intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_Intro_dropdowns/TestNG_Intro.java
@@ -1,12 +1,51 @@
 package com.cydeo.tests.day5_testNG_Intro_dropdowns;
 
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 public class TestNG_Intro {
 
-    @Test
+    @Test (priority = 1)
     public void test1(){
         System.out.println("Test 1 is running...");
+
+        //ASSERT EQUALS: compare 2 same things
+        String actual = "apple";
+        String expected = "apple2";
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test (priority = 2)
+    public void test2(){
+        System.out.println("Test 2 is running...");
+
+        //AssertTrue
+        String actual = "apple";
+        String expected = "apple2";
+        Assert.assertTrue(actual.equals(expected), "your message will go here");
+    }
+
+
+
+    @BeforeClass
+    public void setupMethod(){
+        System.out.println("-----> BeforeClass is running!");
+    }
+
+    @AfterClass
+    public void tearDownClass(){
+        System.out.println("-----> AfterClass is running!");
+    }
+
+    @BeforeMethod
+    public void setUpMethod(){
+        System.out.println("---> BeforeMethod is running!");
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("---> AfterMethod is running!");
     }
 
 
