2c7782a7f691a212107f658c9e15b90d6b3a7699
irfan <23pointalpha@gmail.com>
2022-05-14 16:33:38 +0300

    day5 TestNG_Intro

---
+++ src/test/java/com/cydeo/tests/day5_testNG_Intro_dropdowns/TestNG_Intro.java
@@ -0,0 +1,14 @@
+package com.cydeo.tests.day5_testNG_Intro_dropdowns;
+
+import org.testng.annotations.Test;
+
+public class TestNG_Intro {
+
+    @Test
+    public void test1(){
+        System.out.println("Test 1 is running...");
+    }
+
+
+
+}
