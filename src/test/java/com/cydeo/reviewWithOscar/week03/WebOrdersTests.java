ab3650a69df687ccf9975672002eded545143b63
irfan <23pointalpha@gmail.com>
2022-05-15 20:59:06 +0300

    week03 live review WebOrdersTests class checkboxes, select, delete, VERIFY DELETED!!

---
+++ src/test/java/com/cydeo/reviewWithOscar/week03/WebOrdersTests.java
@@ -0,0 +1,100 @@
+package com.cydeo.reviewWithOscar.week03;
+
+import com.cydeo.utilities.HandleWait;
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class WebOrdersTests {
+
+    WebDriver driver; // declare your variable ----> polymorphic way
+    @BeforeMethod
+    public void loginToWebOrders(){  // the name is UP to you
+        System.out.println("login steps for application");
+        // WebDriver driver = WebDriverFactory.getDriver("chrome"); --> local variable for this method, and CAN NOT be used at the following afterMethod, or Test methods
+        driver = WebDriverFactory.getDriver("chrome"); // instantiate or initiate or create
+
+        //   driver = new FirefoxDriver();  it can point to different browser objects
+        driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test"+ Keys.ENTER); // witout locating submit button we directly click enter
+    }
+    @AfterMethod
+    public void closeBrowser(){
+        System.out.println("closing the browser for following test cases");
+//        driver.close();
+    }
+
+    @Test
+    public void checkBoxTest(){
+        System.out.println("Implementing step 3 and 4 of Test Case 1");
+    /*
+    I need to locate checkAll button then click
+    I need to locate all checkBoxes and verify each of them
+    one by one if they are selected (isSelected())
+locate all checkBoxes: I need to find a locator which will show(point) all the checkboxes, after this step I can use findElements() method for multiple WebElements
+     */
+        //locate checkAll button then click
+        driver.findElement(By.linkText("Check All")).click();
+        ArrayList<WebElement> checkBoxes = (ArrayList<WebElement>) driver.findElements(By.xpath("//input[@type='checkbox']"));
+        for (WebElement eachCheckBox : checkBoxes) {
+            // TestNG comes with Assertion methods
+            Assert.assertTrue(eachCheckBox.isSelected(),"CheckBox IS NOT checked"); // expectation is true
+            //  eachCheckBox.isSelected(); // return type is boolean
+        }
+        // Click on Uncheck All button
+        driver.findElement(By.linkText("Uncheck All")).click();
+        // we need to reLocate the elements again, otherwise we get Stale Element Reference exception
+        checkBoxes = (ArrayList<WebElement>) driver.findElements(By.xpath("//input[@type='checkbox']"));
+        // verify all checkboxes not selected
+        for (WebElement eachCheckBox : checkBoxes) {
+            // eachCheckBox.isSelected() // my boolean expection this time is false
+            Assert.assertFalse(eachCheckBox.isSelected(),"The CheckBox is still selected, we could NOT unSelect all");
+        }
+
+    }
+
+    @Test
+    public void deletePersonTest(){
+        System.out.println("Implementing step 3 and 4 of Test Case 2");
+        String name = "Mark Smith";
+    /*
+    Locate the checkBox using the person's Name: Bob Feather
+    find with name(child)--> go to parent (whole row) --> down to td of checkbox */
+        //td[.='Bob Feather']/../td[1]/input
+        //td[.='Bob Feather']/preceding-sibling::*/input
+        String locatorOfCheckBoxWithName = "//td[.='"+name+"']/../td[1]/input";
+        WebElement checkBox = driver.findElement(By.xpath(locatorOfCheckBoxWithName));
+        checkBox.click();  // select the checkbox with click()
+        HandleWait.staticWait(2);
+
+        // locate delete button and click
+        driver.findElement(By.id("ctl00_MainContent_btnDelete")).click();
+
+        // put some waiting time
+        HandleWait.staticWait(2);
+
+// After we delete, we can get all the names of the customers as a List, then verify that deleted name is NOT in the list
+        // need to get all the names from the table
+        // common locator : //tr//td[2]
+        List<WebElement> namesElements = driver.findElements(By.xpath("//tr//td[2]"));
+        for (WebElement namesElement : namesElements) {
+            // verify each elements text that it does not contain deleted person
+            Assert.assertFalse(namesElement.getText().contains(name));
+        }
+
+    }
+
+
+}
