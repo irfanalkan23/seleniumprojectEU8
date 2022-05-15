f44c84326efeb2edd9d50c1e18f8b4684ca8a85a
irfan <23pointalpha@gmail.com>
2022-05-15 23:14:18 +0300

    day5_testNG_Intro_dropdowns T5_Dropdown

---
+++ src/test/java/com/cydeo/tests/day5_testNG_Intro_dropdowns/T5_Dropdown.java
@@ -0,0 +1,61 @@
+package com.cydeo.tests.day5_testNG_Intro_dropdowns;
+
+import com.cydeo.utilities.HandleWait;
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T5_Dropdown {
+
+    //TC #5: Selecting state from State dropdown and verifying result
+    //1. Open Chrome browser
+    //2. Go to http://practice.cybertekschool.com/dropdown
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://practice.cybertekschool.com/dropdown");
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+//        driver.close();
+    }
+
+    @Test
+    public void testStateDropdown(){
+
+        Select selectState = new Select(driver.findElement(By.xpath("//select[@id='state']")));
+        //3. Select Illinois
+        selectState.selectByIndex(13);
+        HandleWait.staticWait(1);
+
+        //4. Select Virginia
+        selectState.selectByValue("VA");
+        HandleWait.staticWait(1);
+
+        //5. Select California
+        selectState.selectByVisibleText("California");
+        HandleWait.staticWait(1);
+
+        //6. Verify final selected option is California.
+        String expected = "California";
+        String actual = selectState.getFirstSelectedOption().getText();
+        Assert.assertEquals(expected, actual);
+
+        //Use all Select options. (visible text, value, index)
+    }
+
+
+}
